f6e60ee332dcaada2ab510aa77054f1fa425adee
srivadero <rivaderosergio@gmail.com>
2018-07-03 12:46:25 -0300

    ...
    ...

--- src/Form/ProductoType.php
+++ src/Form/ProductoType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Producto;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductoType extends AbstractType
@@ -12,6 +13,11 @@ class ProductoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('fecha', DateTimeType::class, array(
+                'mapped'=> false,
+                'widget' => 'single_text',
+                'attr' => array('class' => 'datetime'),
+            ))
             ->add('nombre')
             ->add('precio')
             ->add('discontinuado')
